e127c2250cdead8109c68ab6b88c931df1effd0a
Павел Фирстов <pavelfirstov@MacBook-Air-Pavel.local>
2026-07-03 21:04:18 -0500

    Updated V3

--- Python.py
+++ Python.py
@@ -1,2 +1,9 @@
-name = input("Please enter your name: ")
-print(f"Hello, {name}!")
+n = input("Number: ")
+n = int(n)
+
+if n < 0:
+    print("Number is negative")
+elif n > 0:
+    print("Number is positive")
+else:
+    print("Number is zero")
